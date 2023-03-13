3b1ab85d168b6dd1f4d6196c1dba6f5ba72dd01e
marcoyireh <pruebatemporal100>
2023-03-13 18:57:56 -0500

    Ultimos cambios

--- RHWebApp/Controllers/HomeController.cs
+++ RHWebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RHWebApp.Models;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace RHWebApp.Controllers
 {
@@ -15,6 +16,12 @@ namespace RHWebApp.Controllers
 
         public IActionResult Index()
         {
+            //string[] x = new string[] { "100", "200", "300" };
+            //var result = ConcatenaArreglo(x);
+            //DateTime fecha = DateTime.ParseExact("01/02/2001", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            int[] numbers = { 2, 3, 4, 5 };
+            var result = ElementosPares(numbers);
+            ViewBag.Message = result;
             return View();
         }
 
@@ -28,5 +35,32 @@ namespace RHWebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public static string ConcatenaArreglo(string[] px)
+        {
+            return String.Join(",", px);
+        }
+
+        public static string Mes(DateTime date)
+        {
+            DateTimeFormatInfo dtinfo = new CultureInfo("es-ES").DateTimeFormat;
+            int month = date.Month;
+            return dtinfo.GetMonthName(month);
+        }
+
+        public static int[] ElementosPares(int[] px)
+        {
+            var squaredNumbers = px.Where(x => x % 2 == 0).ToArray();
+            Array.Sort(px);
+            return squaredNumbers;
+        }
+
+        public static int[] OrdenaArreglo(int[] px)
+        {
+            Array.Sort(px);
+            return px;
+        }
+
+
     }
 }
